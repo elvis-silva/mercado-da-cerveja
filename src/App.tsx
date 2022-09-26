565c2913d851d7829b49d4033266225ef7ac6f08
Elvis Silva <elviscarlossouza@gmail.com>
2022-09-26 11:16:55 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          SITE EM CONSTRUÇÃO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Breve estaremos no ar
+        </p>
       </header>
     </div>
   );
