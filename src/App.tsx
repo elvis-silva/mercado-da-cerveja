afcbcac4e7e92ecab7cec7bc89578e2b7f4eb6f1
Elvis Silva <elviscarlossouza@gmail.com>
2022-09-27 10:51:55 -0300

    update font family and home image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/beer-cup.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="logo" alt="logo" />
+        <h2>Mercado da Cerveja</h2>
         <p>
           SITE EM CONSTRUÇÃO
         </p>
