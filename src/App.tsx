f62a800ca4532ecca99bcd35f3420b5c3de47559
Elvis Silva <elviscarlossouza@gmail.com>
2022-10-02 09:54:16 -0300

    Add planfleto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import logo from './img/beer-cup.png';
-import sula from './img/final-sula.jpg';
+import logo from './img/panfleto.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={sula} className="logo" alt="final copa sul americana" />
-        <p>Vem assistir com a gente sábado 17h</p>
-        <p>Rua Trinta e Dois, 171, D. Pedro II</p>
         <img src={logo} className="logo" alt="logo" />
-        <h2>Mercado da Cerveja</h2>
         <p>
           SITE EM CONSTRUÇÃO
         </p>
